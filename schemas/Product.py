c5d8a61758529061e8e88a9f904613e4f0a41d6a
Victor <vimatoca050106@gmail.com>
2023-06-01 16:45:21 -0500

    update:[schemas->product]created product schema

--- schemas/Product.py
+++ schemas/Product.py
@@ -9,4 +9,14 @@ class Product(BaseModel):
     description: str = Field(max_length=200, min_length=10, description="Description of product")
     price: float = Field(gt=0, description="Price of product")
     entry_date: int = Field(description="entry date")
-    
+    class Config:
+        schema_extra = {
+            "example":{
+                "id":1,
+                "name":"Cerveza Aguila"
+                "brand":"Bavaria"
+                "description":"Bebida alcoholica"
+                "price":5.000
+                "entry_date":1/6/2023
+            }
+        }
