dac60f7f72ae52f71bae048975baf99643e275cf
Victor <vimatoca050106@gmail.com>
2023-06-01 17:03:25 -0500

    update:[schemas->supplier]created supplier schema

---
+++ schemas/Supplier.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class Supplier(BaseModel):
+    """create supplier schema"""
+    id: Optional[int] = None
+    name: str = Field(max_length=30, min_length=1, description="name of supplier")
+    address: str = Field(max_length=20, min_length=1, description="address of supplier")
+    phone: int = Field(max_digits=10,description="phone of supplier")
+    email: str = Field(max_length=30, min_length=1,description="Email of supplier")
