76be9bcfdc0afde33cc932d9769b1f525c0923be
Victor <vimatoca050106@gmail.com>
2023-06-02 10:04:54 -0500

    add[service -> Product]

--- schemas/Product.py
+++ schemas/Product.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, Field
 from typing import Optional
+from pydantic import BaseModel, Field
+import datetime
 
 class Product(BaseModel):
     """create product schemas"""
@@ -8,15 +9,17 @@ class Product(BaseModel):
     brand: str = Field(max_length=30, min_length=1, description="Name of brand")
     description: str = Field(max_length=200, min_length=10, description="Description of product")
     price: float = Field(gt=0, description="Price of product")
-    entry_date: int = Field(description="entry date")
+    entry_date: datetime.date = Field(gen=datetime.date.today(),description="entry date")
+    
     class Config:
+        """example of Product"""
         schema_extra = {
             "example":{
-                "id":1,
-                "name":"Cerveza Aguila"
-                "brand":"Bavaria"
-                "description":"Bebida alcoholica"
-                "price":5.000
-                "entry_date":1/6/2023
+                "id" : 1,
+                "name" : "Cerveza Aguila",
+                "brand" : "Bavaria",
+                "description" : "Bebida alcoholica",
+                "price" : 5.000,
+                "entry_date" : datetime.date.today().strftime('%Y-%m-%d')
             }
         }
