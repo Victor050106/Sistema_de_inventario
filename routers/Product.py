15e77d41cffcf157dc98e4d821e36c41f9239b04
Victor <vimatoca050106@gmail.com>
2023-06-02 10:03:28 -0500

    add[routers ->Product]

--- routers/Product.py
+++ routers/Product.py
@@ -1,43 +1,52 @@
-from fastapi import APIRouter,Path, Query
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
 
-from service.Product import GenresService
-from schemas.Product import Genres 
+from service.Product import ProductService
+from schemas.Product import Product 
 from config.database import Session
 
 
 
-genres_router = APIRouter()
+product_router = APIRouter()
 
-@genres_router.get('/genres_hello', tags=['genres'], status_code=200)
-def get_genres_hello():
-    """function to check the route"""
-    return HTMLResponse('<h1>Hola desde la ruta de genres</h1>')
-
-
-@genres_router.get('/genres', tags = ['genres'], status_code=200)
-def get_genres():
+@product_router.get("/product",tags=["product"],status_code=200)
+def get_product():
     db = Session()
-    result = GenresService(db).get_genres()
-    return JSONResponse(content=jsonable_encoder(result),status_code = 200)
+    result = ProductService(db).get_product()
+    return JSONResponse(content=jsonable_encoder(result),status_code=200)
 
 
-@genres_router.get('/genres_for_id', tags=['genres'], status_code=200)
-def get_genres_for_id(id:int):
+@product_router.post("/product",tags=["product"], status_code=201)
+def create_product(product:Product):
     db = Session()
-    result = GenresService(db).get_for_id(id)
-    return JSONResponse(content=jsonable_encoder(result),status_code = 200)
+    ProductService(db).create_product(product)
+    return JSONResponse(content={"message":"product create successfully","status_code":201})
 
-@genres_router.post('/genres', tags=['genres'], status_code=201)
-def create_genres(genres:Genres):
+
+@product_router.get('/product_for_id',tags=['product'],status_code=200)
+def get_product_for_id(id:int):   
+    db = Session()   
+    result = ProductService(db).get_for_id(id)
+    return JSONResponse (content=jsonable_encoder(result), status_code=200)
+
+
+
+@product_router.put('/product{id}', tags=['product'])
+def update_product(id:int,product:Product):
     db = Session()
-    GenresService(db).create_genre(genres)
-    return JSONResponse(content={"message":"genre created successfully",'status_code':201}, status_code=201)
+    result = ProductService(db).get_for_id(id)
+    if not result:
+        return JSONResponse(content= {"message" : "Product don't found", "status_code":404})
+    ProductService(db).update_product(product)
+    return JSONResponse(content={"message":"product updated succesfully", "status_code":202}, status_code=202)
 
-#creamos un get que trae un solo genero por id 
-
-# para el genres delete debemos verificar que el id existe y despues ese genero lo eliminamos
-
-
+@product_router.delete("/product{id}",tags = ["product"])
+def delete_product(id:int):
+    db = Session()
+    result = ProductService(db).get_for_id(id)
+    if not result:
+        return JSONResponse(content={"message": "Product don't found","status_code":404})
+    ProductService(db).delete_product(id)
+    return JSONResponse(content={"message":"product delete successfully", "status_code":200},status_code=200)
