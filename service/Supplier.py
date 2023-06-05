96b8d6fd2386ed32026539dd913eaff2edce1bc4
Victor <vimatoca050106@gmail.com>
2023-06-05 03:09:15 -0500

    fix [service -> supplier ]

--- service/Supplier.py
+++ service/Supplier.py
@@ -1,43 +1,41 @@
-"""Schemas and models are called"""
-from models.Supplier import Supplier as SupplierModel
+from models.Supplier import Supplier as Suppliermodel
 from schemas.Supplier import Supplier
 
 class SupplierService():
-    """Creating the class SupplierService"""
-    def __init__(self, db) -> None:
+
+    def __init__(self,db) -> None:
         self.db = db
-        
-        
+
     def get_supplier(self):
-        result = self.db.query(SupplierModel).all()
+        result = self.db.query(Suppliermodel).all()
         return result
     
-    def create_supplier(self,supplier:SupplierModel):
-        new_supplier = SupplierModel(
-            supplier_name = supplier.supplier_name.upper(),
-            supplier_address = supplier.supplier_address.upper(),
-            supplier_phone = supplier.supplier_phone.upper(),
-            supplier_email = supplier.supplier_email.upper()
+    def create_supplier(self, supplier:Suppliermodel):
+        new_supplier = Suppliermodel(
+        sub_name=supplier.sub_name,
+        address=supplier.address,
+        phone=supplier.phone,
+        email=supplier.email
         )
-        
         self.db.add(new_supplier)
         self.db.commit()
         return
     
-    def get_for_id(self,id:int):
-        result = self.db.query(SupplierModel).filter(SupplierModel.id == id).first()
+    def update_supplier(self, data:Suppliermodel):
+        supplier = self.db.query(Suppliermodel).filter(Suppliermodel.id == data.id).first()
+        supplier.sub_name = data.sub_name
+        supplier.address = data.address
+        supplier.phone = data.phone
+        supplier.email = data.email
+        self.db.commit()
+        return 
+    
+    def delete_supplier(self, id: int):
+        self.db.query(Suppliermodel).filter(Suppliermodel.id == id).delete()
+        self.db.commit()
+        return 
+    
+    def get_for_id_supplier(self, id:int):
+        result = self.db.query(Suppliermodel).filter(Suppliermodel.id == id).first()
         return result
-    
-    def update_supplier(self,data:Supplier):
-        supplier = self.db.query(SupplierModel).filter(SupplierModel.id == data.id).first()        
-        supplier.supplier_name = data.supplier_name
-        supplier.supplier_address = data.supplier_address
-        supplier.supplier_phone = data.supplier_phone
-        supplier.supplier_email = data.supplier_email
-        self.db.commit()
-        return
-    
-    def delete_supplier(self,id:int):
-        self.db.query(SupplierModel).filter(SupplierModel.id == id).delete()
-        self.db.commit()
-        return
+        
