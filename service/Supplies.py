0f4dd47a22726b40bfd90391ac79573d284d35f4
Victor <vimatoca050106@gmail.com>
2023-06-05 12:49:30 -0500

    fix [service -> Supplies ]

--- service/Supplies.py
+++ service/Supplies.py
@@ -1,39 +1,38 @@
-from models.Supplies import Supplies as SuppliesModel
+from models.Supplies import Supplies as Suppliesmodel
 from schemas.Supplies import Supplies
 
 class SuppliesService():
-    def __init__(self, db) -> None:
+
+    def __init__(self,db) -> None:
         self.db = db
-        
-        
+
     def get_supplies(self):
-        result = self.db.query(SuppliesModel).all()
+        result = self.db.query(Suppliesmodel).all()
         return result
     
-    def create_supplies(self,supplies:SuppliesModel):
-        new_supplies = SuppliesModel(
-            supplier_id = supplies.supplier_id.upper(),
-            product_id = supplies.product_id.upper(),
-            purchase_price = supplies.purchase_price.upper()
+    def create_supplies(self, supplies:Suppliesmodel):
+        new_supplies = Suppliesmodel(
+        supplier_id=supplies.supplier_id,
+        product_id=supplies.product_id,
+        purchase_price=supplies.purchase_price
         )
-        
         self.db.add(new_supplies)
         self.db.commit()
         return
     
-    def get_for_id(self,id:int):
-        result = self.db.query(SuppliesModel).filter(SuppliesModel.id == id).first()
+    def get_for_id(self, id:int):
+        result = self.db.query(Suppliesmodel).filter(Suppliesmodel.id == id).first()
         return result
     
-    def update_supplies(self,data:Supplies):
-        supplies = self.db.query(SuppliesModel).filter(SuppliesModel.id == data.id).first()        
-        supplies.supplier_id = data.supplies_id
-        supplies.product_id = data.product_id
+    def update_supplies(self,id:int, data:Supplies):
+        supplies = self.db.query(Suppliesmodel).filter(Suppliesmodel.id == id).first()
+        supplies.supplier_id = data.supplier_id
+        supplies.product_id = data.product_id 
         supplies.purchase_price = data.purchase_price
         self.db.commit()
         return
     
-    def delete_supplies(self,id:int):
-        self.db.query(SuppliesModel).filter(SuppliesModel.id == id).delete()
+    def delete_supplies(self, id: int):
+        self.db.query(Suppliesmodel).filter(Suppliesmodel.id == id).delete()
         self.db.commit()
         return
