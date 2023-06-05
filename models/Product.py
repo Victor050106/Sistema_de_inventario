79f6d34d9e06ef73eb00e7ef185afc30586a29de
Victor <vimatoca050106@gmail.com>
2023-06-05 02:45:16 -0500

    refactor[ models -> product ]

--- models/Product.py
+++ models/Product.py
@@ -1,15 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column,Integer,String,Float
+
 from config.database import Base
 
-
 class Product(Base):
-    """create Product Model"""
     
     __tablename__ = "product"
     
-    id = Column(Integer, primary_key = True)
-    name =  Column(String)
+    id = Column(Integer,primary_key =True)
+    name = Column(String)
     brand = Column(String)
     description = Column(String)
-    price = Column(float)
-    entry_date = Column(Integer)
+    price = Column(Float)
+    entry_date = Column(String)
+    availability = Column(String)
+    available_quantity = Column(Integer)
