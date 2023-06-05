3648803c59e3f1a4c624ef53098ca8530b925321
Victor <vimatoca050106@gmail.com>
2023-06-05 03:02:59 -0500

    fix [ models -> supplier]

--- models/Supplier.py
+++ models/Supplier.py
@@ -1,15 +1,13 @@
-from sqlalchemy import Column,Integer, String
+from sqlalchemy import Column, Integer, String
 
 from config.database import Base
 
-
 class Supplier(Base):
-    """create Supplier Model"""
-
+    
     __tablename__ = "supplier"
-
-    id = Column(Integer,primary_key = True)
-    Name = Column(String)
-    Address = Column(String)
-    Phone = Column(Integer)
-    Email = Column(String)
+    
+    id = Column(Integer,primary_key=True)
+    sub_name = Column(String)
+    address = Column(String)
+    phone = Column(Integer)
+    email = Column(String) 
